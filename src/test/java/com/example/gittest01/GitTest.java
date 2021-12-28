0f48b8ce58e69d2a01d9af834e2f0311885ce870
Zxyu <18670493901@163.com>
2021-12-28 14:43:33 +0800

    second commit

--- src/test/java/com/example/gittest01/GitTest.java
+++ src/test/java/com/example/gittest01/GitTest.java
@@ -3,5 +3,6 @@ package com.example.gittest01;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git hello");
+        System.out.println("git hello2");
     }
 }
