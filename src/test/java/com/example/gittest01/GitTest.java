3d7c73aca09e656a2c032e4c0576d722206aed44
Zxyu <18670493901@163.com>
2021-12-28 15:02:56 +0800

    hot_fix commit2

--- src/test/java/com/example/gittest01/GitTest.java
+++ src/test/java/com/example/gittest01/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("git hello2");
         System.out.println("git hello3");
         System.out.println("hello git4");
+        System.out.println("hot_fix test");
+
     }
 }
