ad0f2d932b6bd7a2853299bbb729ac2a2cd67d4f
Zxyu <18670493901@163.com>
2021-12-28 15:04:03 +0800

    master commit

--- src/test/java/com/example/gittest01/GitTest.java
+++ src/test/java/com/example/gittest01/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("git hello2");
         System.out.println("git hello3");
         System.out.println("hello git4");
+        System.out.println("master tset");
     }
 }
